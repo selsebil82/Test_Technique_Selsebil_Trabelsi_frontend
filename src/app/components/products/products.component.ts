d5e884bdbb4afb95bd27d03bbc1c23305594d888
Trabelsi Selsebil <selsebil.trabelsi@etudiant-fst.utm.tn>
2023-07-20 09:50:09 +0100

    partie fontend

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -8,42 +8,54 @@ import { Product } from 'src/app/product';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-products:any;
-product=new Product; 
-successMessageId: number | null = null;
+  products: any;
+  originalProducts: any; // Ajoutez cette propriété pour stocker la liste originale des produits
+  product = new Product();
+  successMessageId: number | null = null;
+  searchQuery: string = ''; // Ajoutez cette propriété pour la requête de recherche
 
-  constructor(private dataService:DataService) { }
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
     this.getProductData();
   }
-  getProductData(){
+
+  getProductData() {
     console.log('liste des produits');
-    this.dataService.getProducts().subscribe(res =>{
+    this.dataService.getProducts().subscribe((res) => {
       console.log(res);
       this.products = res;
-    })
-}
-
-insertData(){
-  this.dataService.createProduct(this.product).subscribe(res =>{
-    this.getProductData(); 
-  })
-}
-deleteData(id: any) {
-  if (confirm('Voulez-vous vraiment supprimer ce produit ?')) {
-    this.dataService.deleteProduct(id).subscribe(
-      res => {
-        this.getProductData();
-        this.successMessageId = id; // Stocke l'ID du produit supprimé
-        setTimeout(() => {
-          this.successMessageId = null; // Réinitialise la propriété après un délai de 2 secondes (facultatif)
-        }, 2000);
-      }
-    );
+      this.originalProducts = res; // Stocke la liste originale des produits
+    });
   }
-}
 
+  insertData() {
+    this.dataService.createProduct(this.product).subscribe((res) => {
+      this.getProductData();
+    });
+  }
 
+  deleteData(id: any) {
+    if (confirm('Voulez-vous vraiment supprimer ce produit ?')) {
+      this.dataService.deleteProduct(id).subscribe((res) => {
+        this.getProductData();
+        this.successMessageId = id;
+        setTimeout(() => {
+          this.successMessageId = null;
+        }, 2000);
+      });
+    }
+  }
 
+  onSearchChange() {
+    if (this.searchQuery.trim() !== '') {
+      this.products = this.originalProducts.filter((prod: any) => {
+        const name = prod.name.toLowerCase();
+        const query = this.searchQuery.toLowerCase();
+        return name.includes(query);
+      });
+    } else {
+      this.products = this.originalProducts; // Restaure la liste originale des produits
+    }
+  }
 }
